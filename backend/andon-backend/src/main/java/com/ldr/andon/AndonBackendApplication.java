7476c49fb08553feccabb31a0af799719ccad09d
Cristian <christotal90@gmail.com>
2025-12-13 20:13:21 -0600

    chrome: initial Spring Boot backend with /health for Andon OEE

---
+++ backend/andon-backend/src/main/java/com/ldr/andon/AndonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ldr.andon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndonBackendApplication.class, args);
+	}
+
+}
---
+++ backend/andon-backend/src/test/java/com/ldr/andon/AndonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldr.andon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
